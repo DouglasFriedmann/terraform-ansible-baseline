396eeeb5f7bd8613842cbf9ee57ff2131482eb9d
Douglas Friedmann <douglas.friedmann@gmail.com>
2026-04-13 21:58:13 -0400

    Add Nginx role with templated landing page and update AWS security group to allow HTTP ingress

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,6 +81,14 @@ resource "aws_security_group" "instance" {
   description = "Security group for Ansible baseline instance"
   vpc_id      = aws_vpc.this.id
 
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
